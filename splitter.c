13b09e0ac977a95f5c94b64dd1b1053537675f55
nikolajsamsonow <58106770+nikolajsamsonow@users.noreply.github.com>
2020-08-04 14:58:49 +0300

    Yet better splitter.c

--- splitter.c
+++ splitter.c
@@ -13,7 +13,7 @@ int main (int argc, char **argv)
   const char target_filename[] = ".cur_input";
   size_t target_filename_len = strlen (target_filename);
 
-  const size_t MAX_FILES = 1024;
+  const size_t MAX_FILES = 8192;
 
   int idx = 0;
   for (int i = 1; i < argc; ++i)
@@ -53,20 +53,14 @@ int main (int argc, char **argv)
 
   size_t file_count = 1;
   size_t split_offsets[MAX_FILES];
-  char *tmp_filenames[MAX_FILES];
 
-  /* bytes from the buffer at range [split_offsets[i]; split_offsets[i+1]) will be written into a file with name tmp_filenames[i] */
   split_offsets[0] = 0;
-  tmp_filenames[0] = (char *) malloc (L_tmpnam);
-  tmpnam (tmp_filenames[0]);
   /* slow! */
   for (size_t i = sep_size; i < buf_size - sep_size; ++i)
     {
       if (memcmp (buf + i, sep, sep_size) == 0)
         {
           split_offsets[file_count] = i;
-          tmp_filenames[file_count] = (char *) malloc (L_tmpnam);
-          tmpnam (tmp_filenames[file_count]);
 
           ++file_count;
           i += sep_size;
@@ -79,6 +73,8 @@ int main (int argc, char **argv)
 
   int new_argc = argc - 1 + file_count;
   char **new_argv = (char **) malloc (sizeof (char *) * new_argc);
+  const size_t SUFFIX_BUF_LEN = 16;
+  char suffix_buf[SUFFIX_BUF_LEN];
 
   /* make new argv */
   int argv_offset = 0;
@@ -86,7 +82,9 @@ int main (int argc, char **argv)
     {
       if (i == idx)
         {
-          new_argv[i] = tmp_filenames[0];
+          new_argv[i] = malloc(strlen(argv[idx]) + SUFFIX_BUF_LEN);
+          strcpy(new_argv[i], argv[idx]);
+          strcat(new_argv[i], ".0.o");
           FILE *tmp = fopen (new_argv[i], "wb");
           assert(tmp && "couldn't open a temp file!\n");
           fwrite (buf, 1, split_offsets[1], tmp);
@@ -95,7 +93,10 @@ int main (int argc, char **argv)
           for (int j = 1; j < file_count; ++j)
             {
               ++argv_offset;
-              new_argv[i + argv_offset] = tmp_filenames[j];
+              new_argv[i + argv_offset] = malloc(strlen(argv[idx]) + SUFFIX_BUF_LEN);
+              strcpy(new_argv[i + argv_offset], argv[idx]);
+              sprintf(suffix_buf, ".%d.o", j);
+              strcat(new_argv[i + argv_offset], suffix_buf);
 
               FILE *tmp = fopen (new_argv[i + argv_offset], "wb");
               assert(tmp && "couldn't open a temp file!\n");
@@ -115,8 +116,6 @@ int main (int argc, char **argv)
   /* finally, run the real main */
   int result = the_real_main (new_argc, new_argv);
 
-  for (int i = 0; i < file_count; ++i)
-    remove (tmp_filenames[i]);
   for (int i = 0; i < new_argc; ++i)
     free (new_argv[i]);
   free (new_argv);
